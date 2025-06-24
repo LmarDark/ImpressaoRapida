528c787c22c6646c51b85fdd297600aa43e7932f
LmarDark <lucasmatheusalero@gmail.com>
2025-06-24 11:25:32 -0400

    refactor: rename RoutesTest.php / assertDatabase modified

--- tests/Feature/UserFactoryTest.php
+++ tests/Feature/UserFactoryTest.php
@@ -5,7 +5,9 @@ it('creates a user using the factory', function () {
     $user = User::factory()->create();
 
     $this->assertDatabaseHas('users', [
-        'id' => $user->id,
-        'email' => $user->email,
+        'id'       => $user->id,
+        'name'     => $user->name,
+        'email'    => $user->email,
+        'password' => $user->password,
     ]);
 });
