e2ff4af727454f37df43dc4c89916fcedc383fcc
LmarDark <lucasmatheusalero@gmail.com>
2025-06-24 11:13:50 -0400

    feat: Migrations e Models configurados

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,13 +23,14 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        /*return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+        return [
+            'name'     => fake()->name(),
+            'email'    => fake()->unique()->safeEmail(),
+            'cpf_cnpj' => fake()->unique()->random_int(11, 11),
+            //'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-        ];*/
+        ];
     }
 
     /**
@@ -37,8 +38,8 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        /*return $this->state(fn (array $attributes) => [
+        return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
-        ]);*/
+        ]);
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,21 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        /*Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('cpf_cnpj')->unique();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->rememberToken();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+        // Schema::create('password_reset_tokens', function (Blueprint $table) {
+        //     $table->string('email')->primary();
+        //     $table->string('token');
+        //     $table->timestamp('created_at')->nullable();
+        // });
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
@@ -34,7 +36,7 @@ return new class extends Migration
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
-        });*/
+        });
     }
 
     /**
@@ -43,7 +45,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
+        // Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
